93f0177e97571e583bf26160546bd2941547a93b
danil332 <danil.shtorg111@mail.ru>
2024-07-24 21:14:40 +0700

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 const tg = window.Telegram.WebApp;
 
 
@@ -16,6 +17,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
     <button onClick={onToggleButton}>Toggle</button>
 
     </div>
